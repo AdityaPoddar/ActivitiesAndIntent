8d1d85d4a9020ed3b35f67af23849f38ce9bcf86
Aditya Poddar <adityapoddar.2011@gmail.com>
2022-03-24 19:04:20 +0545

    ActivityandIntent-CodingChallenge1

--- app/src/main/java/com/example/activitesandintent/MainActivity.java
+++ app/src/main/java/com/example/activitesandintent/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.activitesandintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.activitesandintent.extra.MESSAGE";
+    private TextView subHeading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        subHeading=findViewById(R.id.article_subheading);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        String message=subHeading.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 }
