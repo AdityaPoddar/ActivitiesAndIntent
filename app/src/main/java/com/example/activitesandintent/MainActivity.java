26a3f2a9f4cd72821a60d22edf8db6413c5fcf9a
Aditya Poddar <adityapoddar.2011@gmail.com>
2022-03-25 19:06:30 +0545

    CodingChallenge-ActivitiesandIntent

--- app/src/main/java/com/example/activitesandintent/MainActivity.java
+++ app/src/main/java/com/example/activitesandintent/MainActivity.java
@@ -7,28 +7,41 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE =
             "com.example.android.activitesandintent.extra.MESSAGE";
-    private TextView subHeading;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        subHeading=findViewById(R.id.article_subheading);
+
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
 
-        String message=subHeading.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+        switch (view.getId()){
+            case R.id.button_1:
+                intent.putExtra(EXTRA_MESSAGE, getString(R.string.display_one)); startActivity(intent);
+                break;
+            case R.id.button_2:
+                intent.putExtra(EXTRA_MESSAGE, getString(R.string.display_two)); startActivity(intent);
+                break;
+            case R.id.button_3:
+                intent.putExtra(EXTRA_MESSAGE, getString(R.string.display_three)); startActivity(intent);
+                break;
+            default:
+                Toast.makeText(MainActivity.this, "There was an error", Toast.LENGTH_LONG).show();
+                break;
+        }
 
     }
 }
